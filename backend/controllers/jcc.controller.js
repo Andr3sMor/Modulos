02e8acefc61d5ea21c1e83db105e3d786aa48cac
Andres Camilo Camilo <12394190-Andres-Camilo32@users.noreply.replit.com>
2026-03-06 20:27:21 +0000

    jcc axios directo sin proxy

--- backend/controllers/jcc.controller.js
+++ backend/controllers/jcc.controller.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const https = require("https");
 
 const agent = new https.Agent({ rejectUnauthorized: false });
-const JCC_BASE = "https://sgr.jcc.gov.co:8181";
+const JCC_BASE = "https://sgr.jcc.gov.co";
 const JCC_URL = `${JCC_BASE}/apex/f?p=138:1:::NO:::`;
 
 const HEADERS = {
